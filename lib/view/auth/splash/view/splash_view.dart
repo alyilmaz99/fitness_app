e6405b03b2905828022043085d526055a6af5351
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-03-10 23:29:51 +0300

    splash add

--- lib/view/auth/splash/view/splash_view.dart
+++ lib/view/auth/splash/view/splash_view.dart
@@ -1,5 +1,6 @@
 import 'package:fitness_app/view/auth/splash/viewmodel/splash_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,6 +12,15 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends SplashViewModel {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(
+          'assets/animation/loading.json',
+          width: 400,
+          height: 400,
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
   }
 }
--- lib/view/auth/splash/viewmodel/splash_viewmodel.dart
+++ lib/view/auth/splash/viewmodel/splash_viewmodel.dart
@@ -5,7 +5,7 @@ import '../view/splash_view.dart';
 
 abstract class SplashViewModel extends State<SplashScreen> {
   void initState() {
-    Future.delayed(const Duration(seconds: 3), () async {
+    Future.delayed(const Duration(seconds: 5), () async {
       context.pushReplacement('/auth');
     });
   }
